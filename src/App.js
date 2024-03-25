1a259aae1af4f633b2097b2795a61bdb7517daa9
Zoltan Lajtavari <zoltan.lajtavari@idealo.de>
2024-03-25 18:51:32 +0100

    Implements routing.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
+import * as React from "react";
+import {
+    createBrowserRouter, Link, Outlet,
+    RouterProvider,
+} from "react-router-dom";
+
+
+function Album() {
+    return (
+        <div>
+            <SearchBar />
+            <Characters characters={CHARACTERS} />
+        </div>
+    );
+}
 
 function Characters({ characters }) {
     return (
@@ -8,7 +23,6 @@ function Characters({ characters }) {
         </div>
     );
 }
-
 
 function Character({ name, imageUri: imgUri}) {
     return (
@@ -22,24 +36,12 @@ function Character({ name, imageUri: imgUri}) {
     );
 }
 
-
-
 function SearchBar() {
     return (
         <form>
             <input type="text" placeholder="Search..." />
         </form>
     );
-}
-
-function Header() {
-    return (
-        <div>
-            <span>Home</span>
-            <span>Album</span>
-            <span>Favorites</span>
-        </div>
-)
 }
 
 function Tip({ tipOfTheDay }) {
@@ -50,17 +52,57 @@ function Tip({ tipOfTheDay }) {
     )
 }
 
+function PageLayout() {
+    return (
+        <div>
+            <Header />
+            <Outlet />
+        </div>
+    )
+}
+
+const Header = () => {
+    return (
+        <div className='navbar'>
+            <Link to='/'>
+                <h1>Home</h1>
+            </Link>
+            <Link to={'/album'}>
+                <h1>Album</h1>
+            </Link>
+            <Link to={'/favorites'}>
+                <h1>Favorites</h1>
+            </Link>
+        </div>
+    )
+}
+
 const TIPOFTHEDAY = "Don't promise what you can't deliver."
 const CHARACTERS = [{ name: "Glasses Morty", imageUri: "https://rickandmortyapi.com/api/character/avatar/143.jpeg" },
     { name: "Visor Rick", imageUri: "https://rickandmortyapi.com/api/character/avatar/487.jpeg" }]
 
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <PageLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Tip tipOfTheDay={TIPOFTHEDAY}/>,
+            },
+            {
+                path: '/album',
+                element: <Album />,
+            },
+            {
+                path: '/favorites',
+                element: <Album />,
+            },
+        ],
+    },
+])
+
 export default function App() {
-    return (
-        <div>
-            <Header></Header>
-            {/*<Tip tipOfTheDay={TIPOFTHEDAY}></Tip>*/}
-            <SearchBar></SearchBar>
-            <Characters characters={CHARACTERS}></Characters>
-        </div>
-    )
+    return <RouterProvider router={router} />;
 };
