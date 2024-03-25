81f4ff125c9da28463456ab79aa63b5f59e56630
Zoltan Lajtavari <zoltan.lajtavari@idealo.de>
2024-03-25 17:35:13 +0100

    Implements basic structure.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Characters({ characters }) {
+    return (
+        <div>
+            {characters.map((character) => (
+                <Character name={character.name} imageUri={character.imageUri} />
+            ))}
+        </div>
+    );
 }
 
-export default App;
+
+function Character({ name, imageUri: imgUri}) {
+    return (
+        <div>
+            <img src={imgUri} alt="Character"/>
+            <p>{name}</p>
+            <form>
+                <input type="checkbox" />
+            </form>
+        </div>
+    );
+}
+
+
+
+function SearchBar() {
+    return (
+        <form>
+            <input type="text" placeholder="Search..." />
+        </form>
+    );
+}
+
+function Header() {
+    return (
+        <div>
+            <span>Home</span>
+            <span>Album</span>
+            <span>Favorites</span>
+        </div>
+)
+}
+
+function Tip({ tipOfTheDay }) {
+    return (
+        <div>
+            <p>{tipOfTheDay}</p>
+        </div>
+    )
+}
+
+const TIPOFTHEDAY = "Don't promise what you can't deliver."
+const CHARACTERS = [{ name: "Glasses Morty", imageUri: "https://rickandmortyapi.com/api/character/avatar/143.jpeg" },
+    { name: "Visor Rick", imageUri: "https://rickandmortyapi.com/api/character/avatar/487.jpeg" }]
+
+export default function App() {
+    return (
+        <div>
+            <Header></Header>
+            {/*<Tip tipOfTheDay={TIPOFTHEDAY}></Tip>*/}
+            <SearchBar></SearchBar>
+            <Characters characters={CHARACTERS}></Characters>
+        </div>
+    )
+};
